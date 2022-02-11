3c5a38082955be0581592f40ac58824953feea8a
holoto <laibin45@gmail.com>
2022-02-11 23:38:34 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -17,6 +17,6 @@
 #  src-git small8 https://github.com/kenzok8/small-package
 
 # sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-sed -i '$a src-git small-package https://github.com/kenzok8/small-package' feeds.conf.default
+# sed -i '$a src-git small-package https://github.com/kenzok8/small-package' feeds.conf.default
 
-sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+# sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
