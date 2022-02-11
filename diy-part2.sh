742be7f9010e284a09a733279a48ca3b76590b2d
holoto <laibin45@gmail.com>
2022-02-11 23:18:52 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,11 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
+
+
+#  src-git small8 https://github.com/kenzok8/small-package
+
+# sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '$a src-git small-package https://github.com/kenzok8/small-package' feeds.conf.default
+
+sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
