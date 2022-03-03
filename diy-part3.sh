6ba2bf9078066601676ac8a25b8bde2a0954c16e
holoto <laibin45@gmail.com>
2022-03-03 18:06:03 +0800

    Update diy-part3.sh

--- diy-part3.sh
+++ diy-part3.sh
@@ -22,9 +22,9 @@
 
 # echo 'src-git small-package https://github.com/kenzok8/small-package' >> feeds.conf.default
 
-#echo 'src-git kenzo https://github.com/kenzok8/small-package' >> feeds.conf.default
+echo 'src-git kenzo https://github.com/kenzok8/small-package' >> feeds.conf.default
 
-#echo 'src-git small https://github.com/kenzok8/small' >> feeds.conf.default
+echo 'src-git small https://github.com/kenzok8/small' >> feeds.conf.default
 
 # src-git node https://github.com/nxhack/openwrt-node-packages.git;openwrt-18.06
 
