a026d77fdc6a7bd32930f7b1d71d79ebc5bbd3b0
holoto <laibin45@gmail.com>
2022-03-17 23:57:39 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -21,4 +21,10 @@
 
 # sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 # delete all others bcm53xx spec
+
+
+sed -i '$a src-git upx https://github.com/kuoruan/openwrt-upx.git' feeds.conf.default
+
+#git clone https://github.com/kuoruan/openwrt-upx.git package/openwrt-upx
+
 sed -i 's|^TARGET_|# TARGET_|g; s|# TARGET_DEVICES += phicomm_k3|TARGET_DEVICES += phicomm_k3|' target/linux/bcm53xx/image/Makefile
