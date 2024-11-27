5084cf4b7b7065c6b4656997d6c03a7f6124978b
holoto <laibin45@gmail.com>
2024-11-27 13:14:50 +0100

    Update diy-part3.sh

--- diy-part3.sh
+++ diy-part3.sh
@@ -22,11 +22,11 @@
 
 # echo 'src-git small-package https://github.com/kenzok8/small-package' >> feeds.conf.default
 
-echo 'src-git kenzo https://github.com/kenzok8/small-package' >> feeds.conf.default
+# echo 'src-git kenzo https://github.com/kenzok8/small-package' >> feeds.conf.default
 
 echo 'src-git kwrt https://github.com/kiddin9/kwrt-packages' >> feeds.conf.default
 
-echo 'src-git small https://github.com/kenzok8/small' >> feeds.conf.default
+# echo 'src-git small https://github.com/kenzok8/small' >> feeds.conf.default
 
 
 # sed -i '$a src-git upx https://github.com/kuoruan/openwrt-upx.git' feeds.conf.default
