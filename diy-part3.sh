c669162f41e80e16640a8280492b6e5f865bda6d
holoto <laibin45@gmail.com>
2024-11-27 11:02:50 +0100

    Update diy-part3.sh

--- diy-part3.sh
+++ diy-part3.sh
@@ -24,7 +24,7 @@
 
 echo 'src-git kenzo https://github.com/kenzok8/small-package' >> feeds.conf.default
 
-echo 'src-git kwrt-packages https://github.com/kiddin9/kwrt-packages' >> feeds.conf.default
+echo 'src-git kwrt https://github.com/kiddin9/kwrt-packages' >> feeds.conf.default
 
 echo 'src-git small https://github.com/kenzok8/small' >> feeds.conf.default
 
